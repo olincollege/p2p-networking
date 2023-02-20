b970c4093743f4d0b5cadabf041ab707574826d5
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-02-20 00:05:26 -0500

    Hash map implementation (#5)
    * core dumped
    
    * fix core dump
    
    * add header
    
    * memoery leak
    
    * fixed memoery leaks
    
    * reduce test size
    
    * typo

--- src/vector.h
+++ src/vector.h
@@ -1,7 +1,8 @@
+#pragma once 
+
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
-#pragma clang diagnostic ignored "-Wstrict-prototypes"
 
 // NOLINTBEGIN -- false warnings from strict-prototypes and macro () enclosure also memcpy warning is non-relevant
 #define vector__(TYPE, NAME)                                              \
@@ -13,7 +14,7 @@
     } vector_##NAME;                                                      \
                                                                           \
     /* Inserts an element to the end of the vector */                     \
-    void push_vec_##NAME(vector_##NAME* vec, TYPE element) {              \
+    static void push_vec_##NAME(vector_##NAME* vec, TYPE element) {       \
         if(vec->size == vec->alloc) {                                     \
             TYPE* new_arr = malloc(sizeof(TYPE) * vec->alloc * 2);        \
             memcpy(new_arr, vec->arr, sizeof(TYPE) * vec->alloc * 2);     \
@@ -25,24 +26,18 @@
         vec->arr[vec->size-1] = element;                                  \
     }                                                                     \
     /* Returns a pointer to the alement at a given index in the vector */ \
-    TYPE* get_vec_##NAME(vector_##NAME* vec, size_t idx) {                \
+    static TYPE* get_vec_##NAME(vector_##NAME* vec, size_t idx) {         \
         return &vec->arr[idx];                                            \
     }                                                                     \
     /* Creates a vector container with no elements in it */               \
-    vector_##NAME new_vec_##NAME() {                                      \
+    static vector_##NAME new_vec_##NAME() {                               \
         vector_##NAME new_struct = {malloc(sizeof(TYPE)), 0, 1};          \
         return new_struct;                                                \
     }                                                                     \
     /* De-allocates all dynamic memory used by the container */           \
-    void free_vec_##NAME(vector_##NAME *vec) {                            \
+    static void free_vec_##NAME(vector_##NAME *vec) {                     \
         free(vec->arr);                                                   \
         vec->arr = NULL;                                                  \
     }                                                                     \
 // NOLINTEND
-
-// define common vector types we might need
-vector__(int, int)
-vector__(char, char)
-vector__(int, intptr)
-vector__(char*, charptr)
 
---
+++ test/test_hash_table.c
@@ -0,0 +1,75 @@
+#include <criterion/criterion.h>
+#include <criterion/new/assert.h>
+
+#include "../src/hash_table.h"
+
+// NOLINTBEGIN -- the magic constant warnings are useless here
+Test(test_map, simple_test) {
+    hash_table table = make_table();
+    char* key1 = "key1";
+    char* key2 = "key2";
+    int val1 = 1;
+    int val2 = 2;
+    set_value(&table, key1, &val1, sizeof(val1));
+    set_value(&table, key2, &val2, sizeof(val2));
+
+    kv_pair* kv1 = get_kv_pair(&table, key1); 
+    int res1 = *(int*)kv1->value;
+    cr_assert(eq(int, val1, res1));
+
+    kv_pair* kv2 = get_kv_pair(&table, key2); 
+    int res2 = *(int*)kv2->value;
+    cr_assert(eq(int, val2, res2));
+
+    hash_dealloc(&table);
+}
+
+// Tested with valgrind against memory leaks
+Test(test_map, advance_test) {
+    for(size_t total_iter = 0; total_iter < 1000; total_iter++) {
+        size_t iters = 26;
+        hash_table table = make_table();
+        for(size_t idx = 0; idx < iters; idx++) {
+            char key[4] = "abc";
+            key[3] = 'a' + idx;
+            key[4] = '\0';
+            set_value(&table, key, &idx, sizeof(idx));
+        }
+        for(size_t idx = 0; idx < iters; idx++) {
+            char key[4] = "abc";
+            key[3] = 'a' + idx;
+            key[4] = '\0';
+            kv_pair* kv = get_kv_pair(&table, key); 
+            int actual_value = *(int*)kv->value;
+            cr_assert(eq(int, idx, actual_value));
+        }
+        hash_dealloc(&table);
+    }
+}
+
+// Tested with valgrind against memory leaks
+Test(test_map, memory_leak_test) {
+    for(size_t iter = 0; iter < 10000; iter++) {
+        hash_table table = make_table();
+        char* key1 = "key1";
+        char* key2 = "key2";
+        int val1 = 1;
+        int val2 = 2;
+        set_value(&table, key1, &val1, sizeof(val1));
+        set_value(&table, key2, &val2, sizeof(val2));
+
+        kv_pair* kv1 = get_kv_pair(&table, key1); 
+        int res1 = *(int*)kv1->value;
+        cr_assert(eq(int, val1, res1));
+
+        kv_pair* kv2 = get_kv_pair(&table, key2); 
+        int res2 = *(int*)kv2->value;
+        cr_assert(eq(int, val2, res2));
+
+        hash_dealloc(&table);
+    }
+}
+
+
+
+// NOLINTEND
