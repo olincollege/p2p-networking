a2704323d8ed028698ff3973c684682e3f10db99
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-02-20 13:39:45 -0500

    rebase branch onto main (#7)

--- src/network.c
+++ src/network.c
@@ -6,6 +6,18 @@
 #include <sys/epoll.h>
 #include "./network.h"
 
+uint64_t as_epoll_data(int32_t fd, int32_t type) {
+    epoll_custom_data event_d = {fd, type};
+    uint64_t result;  
+    memcpy(&result, &event_d, sizeof(uint64_t));
+    return result;
+}
+epoll_custom_data as_custom_data(uint64_t epoll_data_result) {
+    epoll_custom_data result = {(int)0, (int)0};
+    memcpy(&result, &epoll_data_result, sizeof(uint64_t));
+    return result;
+}
+
 /* Makes a fcntl system call to mark a socket a non-blocking
  * We want to be able to handle multiple IO operations at the same time with epoll
  * https://man7.org/linux/man-pages/man2/fcntl.2.html
@@ -25,11 +37,12 @@ void non_blocking_socket(int socket) {
 
 /* Creates a TCP socket and binds it to port */
 int create_socket() {
-    const struct sockaddr_in server_adress = {
+    struct sockaddr_in6 server_adress = {
         AF_INET6,                       // use ipv6 resolution
-        (sa_family_t) LISTEN_PORT,      // port to listen on  
-        inet_addr("127.0.0.1")          // listen on localhost
-    };
+        htons(LISTEN_PORT),             // port to listen on  
+        0,                              // null pad for inet_pton command
+    }; 
+    inet_pton(AF_INET6, "::1", &server_adress.sin6_addr); // listen on localhost
 
     // try to allocate a TCP socket from OS
     int server_socket = socket(AF_INET6, SOCK_STREAM, 0);
@@ -37,7 +50,7 @@ int create_socket() {
         puts("failed to allocate socket");
         exit(1);
     }
-    
+
     // try to bind socket to port 
     int bind_res = bind(server_socket, (const struct sockaddr*) &server_adress, sizeof(server_adress));
     if(bind_res < 0) {
@@ -71,7 +84,7 @@ int create_epoll_socket() {
 
     // wrap server socket into an epoll event 
     struct epoll_event server_epoll; 
-    server_epoll.data.fd = EPOLL_LISTEN_FD; 
+    server_epoll.data.u64 = as_epoll_data(server_socket, EPOLL_LISTEN_FD); 
 
     // https://man7.org/linux/man-pages/man7/epoll.7.html
     // mark as edge-triggering with I/O in
@@ -80,6 +93,6 @@ int create_epoll_socket() {
         puts("failed to bind socket to epoll descriptor");
         exit(1);
     }
-    
+
     return epoll_descriptor;
 }
