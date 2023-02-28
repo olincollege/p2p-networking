5d5cb579c0160f3979fb4364fb1467b81de4127a
Devlin Ih <dih@olin.edu>
2023-02-28 13:32:32 -0500

    Fix issue where a hashtable in a struct was referenced as a pointer

--- src/state.c
+++ src/state.c
@@ -6,19 +6,19 @@
 #include "./state.h"
 
 void add_piece_want(client_state state, unsigned long hash) {
-  set_value(&state->pieces_want, &hash, sizeof(unsigned long), NULL, 1);
+  set_value(&state.pieces_want, &hash, sizeof(unsigned long), NULL, 1);
 }
 
 void remove_piece_want(client_state state, unsigned long hash) {
-  remove_kv_pair(&state->pieces_want, &hash, sizeof(unsigned long));
+  remove_kv_pair(&state.pieces_want, &hash, sizeof(unsigned long));
 }
 
 void add_client_connected(client_state state, connected_client client) {
-  set_value(&state->clients_connected, &client, sizeof(struct connected_client),
+  set_value(&state.clients_connected, &client, sizeof(struct connected_client),
             NULL, 1);
 }
 
 void remove_client_connected(client_state state, connected_client client) {
-  remove_kv_pair(&state->clients_connected, &client,
+  remove_kv_pair(&state.clients_connected, &client,
                  sizeof(struct connected_client));
 }
