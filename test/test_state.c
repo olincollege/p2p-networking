2d1162a74ecdd5019e27e23d86224590458604b6
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-03-01 18:26:08 -0500

    add assertions

--- test/test_state.c
+++ test/test_state.c
@@ -21,41 +21,62 @@ Test(test_state, test_random_state) {
 }
 
 Test(test_state, test_peer_exhange) {
-   remove("/tmp/test_peer_exchange");
-   int fd_in = open("/tmp/test_peer_exchange", O_CREAT | O_APPEND | O_RDWR, S_IRUSR | S_IWUSR);    
-   if(fd_in < 0) {
+    remove("/tmp/test_peer_exchange");
+    int fd_in = open("/tmp/test_peer_exchange", O_CREAT | O_APPEND | O_RDWR, S_IRUSR | S_IWUSR);    
+    if(fd_in < 0) {
         puts("failed to open test_state fd");
-   }
+    }
 
-   // say we know about 4 random ports
-   client_state state = new_state();
-   add_port(&state, 1);
-   add_port(&state, 2);
-   add_port(&state, 3);
-   add_port(&state, 4);
+    // say we know about 4 random ports
+    client_state state = new_state();
+    add_port(&state, 1);
+    add_port(&state, 2);
+    add_port(&state, 3);
+    add_port(&state, 4);
 
-   // say we are connected to a "client" mocked as stdin
-   add_file_descriptor(&state, fd_in);
+    // say we are connected to a "client" mocked as stdin
+    add_file_descriptor(&state, fd_in);
 
-   // attempt to broadcast to fds
-   peer_exchange(&state);
-   close(fd_in);
+    // attempt to broadcast to fds
+    peer_exchange(&state);
+    close(fd_in);
 
-   // assert against the written message
-   char* buffer = malloc(1024*1024); 
-   size_t bytes_read = 0; 
-   FILE* fd_read = fopen("/tmp/test_peer_exchange", "r");
-   while(!feof(fd_read)) {
-     char ch = fgetc(fd_read); 
-     buffer[bytes_read] = ch;
-     bytes_read++;
-   }
-   fclose(fd_read);
-   peer_message* message = malloc(bytes_read);
-   memcpy(message, buffer, bytes_read);
+    // recover the written message
+    char* buffer = malloc(1024*1024); 
+    size_t bytes_read = 0; 
+    FILE* fd_read = fopen("/tmp/test_peer_exchange", "r");
+    while(1) {
+        char ch = fgetc(fd_read); 
+        if(feof(fd_read)) {
+            break;
+        }
+        buffer[bytes_read] = ch;
+        bytes_read++;
+    }
+    fclose(fd_read);
+    peer_message* message = malloc(bytes_read);
+    memcpy(message, buffer, bytes_read);
 
-   free(message);
-   free(buffer);
+    // assert against the written message
+    cr_assert(eq(int, sizeof(peer_message) + 4*sizeof(peer_info), bytes_read));
+    cr_assert(eq(int, 2, (int)message->type)); 
+
+    in_port_t port_1 = message->peers[0].addr_port;
+    in_port_t port_2 = message->peers[1].addr_port;
+    in_port_t port_3 = message->peers[2].addr_port;
+    in_port_t port_4 = message->peers[3].addr_port;
+
+
+    // ordering not guaranteed by the hash_map
+    cr_assert(eq(int, 1, port_1 == 1 || port_2 == 1 || port_3 == 1 || port_4 == 1)); 
+    cr_assert(eq(int, 1, port_1 == 2 || port_2 == 2 || port_3 == 2 || port_4 == 2)); 
+    cr_assert(eq(int, 1, port_1 == 3 || port_2 == 3 || port_3 == 3 || port_4 == 3)); 
+    cr_assert(eq(int, 1, port_1 == 4 || port_2 == 4 || port_3 == 4 || port_4 == 4)); 
+
+    // cleanup
+    free(message);
+    free(buffer);
+    dealloc_state(&state);
 }
 
 // NOLINTEND
