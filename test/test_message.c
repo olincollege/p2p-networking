305bcdef0172ee169263e4e3f462f859fcc43b92
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-03-03 16:16:45 -0500

    fix static assert

--- test/test_message.c
+++ test/test_message.c
@@ -8,7 +8,7 @@ Test(message, assert_message) {
   // make sure that no weird things are happening with struct alignment or
   // repacking by the compiler
   _Static_assert(sizeof(peer_info) == 18, "peer info should be 18 bytes");
-  _Static_assert(sizeof(ask_message) == 38, "ask_message should be 38 bytes");
+  _Static_assert(sizeof(ask_message) == 37, "ask_message should be 37 bytes");
   _Static_assert(sizeof(give_message) == 131109,
                  "give message should be 131109 bytes");
 }
