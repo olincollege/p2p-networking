8914fc717545a2b65a887f0c688e5bbe68e3bbb2
Devlin Ih <dih@olin.edu>
2023-03-07 23:07:41 -0500

    Add documentation comments for hash_table functions

--- src/hash_table.h
+++ src/hash_table.h
@@ -20,28 +20,63 @@ vector__(kv_pair, kv_pair)
   size_t num_elements;
 } hash_table;
 
-/* Sets a value in the hash table. The key and value is copied by value. */
+/**
+ * Set a value in the hash table. The key and value is copied by value.
+ *
+ * @param in_table Location of the hash table in memory
+ * @param key Location of binary data to hash as a key
+ * @param key_size Number of bytes the key occupies in memory
+ * @param value Location of data to copy into the hash table
+ * @param value_size Number of bytes the value occupies in memory
+ */
 void set_value(hash_table *in_table, void *key, size_t key_size, void *value,
                size_t value_size);
 
-/* Removes a key from the hash_table or does nothing is such key does not exist
+/**
+ * Remove a key/value pair from the hash table.
+ *
+ * If key is not in the hash table, does nothing.
+ *
+ * @param in_table Location of the hash table in memory
+ * @param key Location of binary data to hash as a key
+ * @param key_size Number of bytes the key occupies in memory
  */
 void remove_kv_pair(hash_table *in_table, void *key, size_t key_size);
 
-/* Returns a key/value pair in the hash_table or a NULL padded struct if it does
- * not exist */
+/**
+ * Return a key/value pair in the hash_table.
+ *
+ * If key/value pair does not exist, returns a NULL padded struct.
+ *
+ * @param in_table Location of the hash table in memory
+ * @param key Location of binary data to hash as a key
+ * @param key_size Number of bytes the key occupies in memory
+ */
 kv_pair *get_kv_pair(hash_table *in_table, void *key, size_t key_size);
 
-/* Returns a vector with all of the key/value pairs.
+/**
+ * Return a vector with all of the key/value pairs.
+ *
  * Note that the keys and values are pointers and may be invalidated with any
  * future hash_table operations.
+ *
+ * @param in_table Location of the hash table in memory
+ * @return A vector with all the key/value pairs currently in the hash table.
  */
 vector_kv_pair collect_table(hash_table *in_table);
 
-/* Create a new hable */
+/**
+ * Create a new hash table.
+ *
+ * @return An initialized, empty hash table object.
+ */
 hash_table make_table();
 
-/* Free data used by the hash table */
+/**
+ * Free data used by the hash table.
+ *
+ * @param in_table Location of the hash table in memory
+ */
 void hash_dealloc(hash_table *in_table);
 
 /* Internal */
